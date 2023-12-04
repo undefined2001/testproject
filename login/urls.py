c021681d76187fde44f4d47006c32e591bf5e40f
Jobair Al Sarkar <jobairalsarkar338@gmail.com>
2023-12-04 06:04:12 +0600

    Requesting for advising, adding course, approving and declining by faculies and overlapping features are added in this stage of commit.

--- login/urls.py
+++ login/urls.py
@@ -16,23 +16,33 @@ urlpatterns = [
     path('staff-profile/', views.staffProfile, name='staffProfile'),
     path('faculty-add-course/', views.add_course, name="faculty_add_course"),
     path('faculty-see-courses', views.see_course_list, name='faculty_see_courses'),
-    path('faculty-delete-course/<int:id>', views.delete_course, name="faculty_delete_course"),
-    path('faculty-make-announcement', views.faculty_announcements, name="faculty_make_announcement"),
+    path('faculty-delete-course/<int:id>',
+         views.delete_course, name="faculty_delete_course"),
+    path('faculty-make-announcement', views.faculty_announcements,
+         name="faculty_make_announcement"),
 
-    path('student-announcement-view/', views.studentAnnouncementView, name='studentAnnouncementView'),
-    path('staff-announcement-view/', views.staffAnnouncementView, name='staffAnnouncementView'),
+    path('student-announcement-view/', views.studentAnnouncementView,
+         name='studentAnnouncementView'),
+    path('staff-announcement-view/', views.staffAnnouncementView,
+         name='staffAnnouncementView'),
     path('post-announcement/', views.postAnnouncement, name='postAnnouncement'),
 
-    path('complain-student-view/', views.complainStudentView, name='complainStudentView'),
+    path('complain-student-view/', views.complainStudentView,
+         name='complainStudentView'),
     path('complain-staff-view/', views.complainStaffView, name='complainStaffView'),
     path('post-complain/', views.postComplain, name='postComplain'),
 
 
-    #---------------------------------------------------#
-    path('approval-staff-view/', views.staff_pending_request_view, name="approve_request_pending"),
-    path('approve-student/<int:id>', views.approve_student, name="approve_request_confirmation"),
+    # ---------------------------------------------------#
+    path('approval-staff-view/', views.staff_pending_request_view,
+         name="approve_request_pending"),
+    path('approve-student/<int:id>', views.approve_student,
+         name="approve_request_confirmation"),
 
-    #-----------------Module 4-----------------------#
+    # -----------------Module 4-----------------------#
     path('staff-add-faculty/', views.add_faculty, name='staff_add_faculty'),
+    path('student_advising/', views.student_advising, name="student_advising"),
+    path('make_request/', views.make_request, name='make_request'),
+    path('request_list/', views.request_list, name='request_list'),
 
 ]
